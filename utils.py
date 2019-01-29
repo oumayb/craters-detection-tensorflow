67a2a4e01b557d803d650df441fc107836f0d0c9
oumayb <oumayma.bounou@telecom-paristech.fr>
2019-01-29 15:59:03 +0100

    Added function to create binary labels whether there is a crater or not

--- utils.py
+++ utils.py
@@ -19,3 +19,28 @@ def show_images(n_images, imgs, cmap=None):
         plt.figure()
         plt.imshow(imgs[idx], cmap=cmap)
         plt.title("Img n° {}".format(idx))
+
+
+def binarize_y_classification(y):
+    """
+    Transorms dataframes craters positions labels to nupy arrays for classification, 1 if there is crater, 0 ow
+
+    Params
+    ------
+    y: `panas.DataFrame`
+        contains i	row_p	col_p	radius_p information
+
+    Returns
+    -------
+    y_classification: `numpy array`
+        array of the size of the number of images, 1 if there is a crater, 0 ow
+    """
+    y_classification = y.copy()
+    y_classification["has_crater"] = 1
+    y_classification = y_classification.drop_duplicates(subset="i")
+    y_classification = y_classification[["i", "has_crater"]]
+    y_classification = y_classification.set_index("i")
+    new_index = list(range(7500))
+    y_classification = y_classification.reindex(new_index, fill_value=0)
+
+    return y_classification.values.flatten()
